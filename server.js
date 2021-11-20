3bdc9b2b39ea70b3ec5f6978886fee93cb553bd0
Igboanugwo Collins <munisco12@gmail.com>
2021-11-20 23:29:30 +0100

    updated to only rq1 details

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-    res.send('Welcome to rq1-backend current name -- roqquappchat')
+    res.send('Welcome to rq1-back current name -- roqquappchat')
 })
 
  const transporter = nodemailer.createTransport({
