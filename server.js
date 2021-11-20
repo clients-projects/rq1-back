063476814c1d8dfbc5d9d58960e28732eeef3f60
Igboanugwo Collins <munisco12@gmail.com>
2021-11-20 23:31:21 +0100

    updated to only rq1 details

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Origin', 'https://roqquappchat.com')
     res.setHeader(
         'Access-Control-Allow-Methods',
         'OPTIONS, GET, POST, PUT, PATCH, DELETE'
