8c3267127334321a458b9dc856892a0b882e4272
Igboanugwo Collins <munisco12@gmail.com>
2021-11-22 16:49:40 +0100

    added csrf

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get('/', (req, res) => {
 
 app.get('/form', csrfProtection, (req, res) => {
     console.log('in form')
-    res.render({ csrfToken: req.csrfToken() })
+    res.send({ csrfToken: req.csrfToken() })
 })
 
 app.post('/rq-1', parseForm, csrfProtection, (req, res, next) => {
