da75e0d3b428b048704c05397c9c60d94016dec0
Igboanugwo Collins <munisco12@gmail.com>
2021-11-22 18:48:07 +0100

    added csrf

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 
-// const sessionConfig = {
-//     // ... other methods
-//     cookie: {
-//         sameSite: 'none',
-//     },
-// }
+const sessionConfig = {
+    // ... other methods
+    cookie: {
+        sameSite: 'none',
+    },
+}
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.set('trust proxy', 1) // trust first proxy
-//     sessionConfig.cookie.secure = true // serve secure cookies
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.set('trust proxy', 1) // trust first proxy
+    sessionConfig.cookie.secure = true // serve secure cookies
+}
 
 app.use(session(sessionConfig))
 
